c589c01dc2dcdc38878cff7b878ea140bb9349f6
RodrigoBrum1989 <rodrigoocbrum@outlook.com>
2021-12-01 01:47:21 -0300

    Commit pra quebrar versão
    Commit pra quebrar versão

--- src/main/java/br/com/enade/dao/DAO.java
+++ src/main/java/br/com/enade/dao/DAO.java
@@ -8,7 +8,7 @@ import javax.persistence.criteria.CriteriaQuery;
 /**
  *
  * @author claud
- */
+ 
 public class DAO<T> implements Serializable {
 
     private static final long serialVersionUID = 1L;
